049f4127b2257eb1e02c2620a385b09fce76de2e
Carlos Mendoza <inhack20@gmail.com>
2016-09-01 23:07:38 -0400

    Agregando copia base

---
+++ Service/SequenceGenerator/Adapter/DoctrineORMAdapter.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the BtoB4Rewards package.
+ * 
+ * (c) www.btob4rewards.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnoready\Commom\Service\SequenceGenerator\Adapter;
+
+/**
+ * Description of DoctrineORMAdapter
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class DoctrineORMAdapter implements SequenceGeneratorAdapter 
+{
+    /**
+     *
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+}
---
+++ Service/SequenceGenerator/Adapter/SequenceGeneratorAdapter.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the BtoB4Rewards package.
+ * 
+ * (c) www.btob4rewards.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnoready\Commom\Service\SequenceGenerator\Adapter;
+
+/**
+ * Adaptador de generacion de secuencia
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+interface SequenceGeneratorAdapter {
+    //put your code here
+}
