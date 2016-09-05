793cb0c778ac358fae9e5d72a9209f0b239737d2
Carlos Mendoza <inhack20@gmail.com>
2016-09-04 23:00:59 -0400

    Agregando test

---
+++ Tests/Service/SequenceGenerator/SequenceGeneratorServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the BtoB4Rewards package.
+ * 
+ * (c) www.btob4rewards.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Prueba el generador de secuencia
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class SequenceGeneratorServiceTest extends TestCase {
+    public function testNewService() {
+//        $sequenceGeneratorService = new \Tecnoready\Common\Service\SequenceGenerator\SequenceGeneratorService();
+//        
+//        $mask = "FAC-{00000}";
+//        $field = "ref";
+//        
+//        $adapter = new \Tecnoready\Common\Service\SequenceGenerator\Adapter\Yii2ActiveRecordAdapter();
+//        $sequenceGeneratorService->setAdapter($adapter);
+//        $sequenceGeneratorService->setClassMap([
+//            \client\advertising\modules\core\models\billing\Order::class => ["mask" => $mask]
+//        ]);
+//        $query = \client\advertising\modules\core\models\billing\Order::find();
+//        $adapter->setQb($query);
+//        
+//        $next = $sequenceGeneratorService->generateNext(new \client\advertising\modules\core\models\billing\Order, $mask, $field);
+//        $next = $sequenceGeneratorService->setRef(new \client\advertising\modules\core\models\billing\Order);
+        
+        $this->assertEquals(-1,-1);
+    }
+}
