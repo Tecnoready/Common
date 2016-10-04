1cc134f04fdcae85446108909d092825346d48c8
Carlos Mendoza <inhack20@gmail.com>
2016-10-04 01:37:28 -0400

    Mejora de sql

--- Resources/data/statistics/pgsql.sql
+++ Resources/data/statistics/pgsql.sql
@@ -1,76 +1,87 @@
 begin;
-CREATE TABLE "statistics_monthly" (
-id bigserial NOT NULL,
-"year" int4 NOT NULL,
-"month" int4 NOT NULL,
-"total" varchar(255) NOT NULL,
-"day1" varchar(255) NOT NULL,
-"day2" varchar(255) NOT NULL,
-"day3" varchar(255) NOT NULL,
-"day4" varchar(255) NOT NULL,
-"day5" varchar(255) NOT NULL,
-"day6" varchar(255) NOT NULL,
-"day7" varchar(255) NOT NULL,
-"day8" varchar(255) NOT NULL,
-"day9" varchar(255) NOT NULL,
-"day10" varchar(255) NOT NULL,
-"day11" varchar(255) NOT NULL,
-"day12" varchar(255) NOT NULL,
-"day13" varchar(255) NOT NULL,
-"day14" varchar(255) NOT NULL,
-"day15" varchar(255) NOT NULL,
-"day16" varchar(255) NOT NULL,
-"day17" varchar(255) NOT NULL,
-"day18" varchar(255) NOT NULL,
-"day19" varchar(255) NOT NULL,
-"day20" varchar(255) NOT NULL,
-"day21" varchar(255) NOT NULL,
-"day22" varchar(255) NOT NULL,
-"day23" varchar(255) NOT NULL,
-"day24" varchar(255) NOT NULL,
-"day25" varchar(255) NOT NULL,
-"day26" varchar(255) NOT NULL,
-"day27" varchar(255) NOT NULL,
-"day28" varchar(255) NOT NULL,
-"day29" varchar(255) NOT NULL,
-"day30" varchar(255) NOT NULL,
-"day31" varchar(255) NOT NULL,
-"created_at" timestamp NOT NULL,
-"updated_at" timestamp NOT NULL,
-"deleted_at" timestamp DEFAULT NULL,
-"created_from_ip" varchar(45) DEFAULT NULL,
-"updated_from_ip" varchar(45) DEFAULT NULL,
-"yearEntity_id" bigint NOT NULL,
-PRIMARY KEY ("id") 
+CREATE TABLE statistics_year
+(
+  id bigserial NOT NULL,
+  year integer NOT NULL,
+  total character varying(255) NOT NULL,
+  total_month_1 character varying(255) NOT NULL,
+  total_month_2 character varying(255) NOT NULL,
+  total_month_3 character varying(255) NOT NULL,
+  total_month_4 character varying(255) NOT NULL,
+  total_month_5 character varying(255) NOT NULL,
+  total_month_6 character varying(255) NOT NULL,
+  total_month_7 character varying(255) NOT NULL,
+  total_month_8 character varying(255) NOT NULL,
+  total_month_9 character varying(255) NOT NULL,
+  total_month_10 character varying(255) NOT NULL,
+  total_month_11 character varying(255) NOT NULL,
+  total_month_12 character varying(255) NOT NULL,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  deleted_at timestamp without time zone,
+  created_from_ip character varying(45) DEFAULT NULL::character varying,
+  updated_from_ip character varying(45) DEFAULT NULL::character varying,
+  CONSTRAINT statistics_year_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
 );
 
-CREATE INDEX "IDX_DA2703A94DFDCFB" ON "statistics_monthly" ("yearEntity_id");
-
-CREATE TABLE "statistics_year" (
-id bigserial NOT NULL,
-"year" int4 NOT NULL,
-"total" varchar(255) NOT NULL,
-"total_month_1" varchar(255) NOT NULL,
-"total_month_2" varchar(255) NOT NULL,
-"total_month_3" varchar(255) NOT NULL,
-"total_month_4" varchar(255) NOT NULL,
-"total_month_5" varchar(255) NOT NULL,
-"total_month_6" varchar(255) NOT NULL,
-"total_month_7" varchar(255) NOT NULL,
-"total_month_8" varchar(255) NOT NULL,
-"total_month_9" varchar(255) NOT NULL,
-"total_month_10" varchar(255) NOT NULL,
-"total_month_11" varchar(255) NOT NULL,
-"total_month_12" varchar(255) NOT NULL,
-"created_at" timestamp NOT NULL,
-"updated_at" timestamp NOT NULL,
-"deleted_at" timestamp DEFAULT NULL,
-"created_from_ip" varchar(45) DEFAULT NULL,
-"updated_from_ip" varchar(45) DEFAULT NULL,
-PRIMARY KEY ("id") 
+CREATE TABLE statistics_monthly
+(
+  id bigserial NOT NULL,
+  year integer NOT NULL,
+  month integer NOT NULL,
+  total character varying(255) NOT NULL,
+  day1 character varying(255) NOT NULL,
+  day2 character varying(255) NOT NULL,
+  day3 character varying(255) NOT NULL,
+  day4 character varying(255) NOT NULL,
+  day5 character varying(255) NOT NULL,
+  day6 character varying(255) NOT NULL,
+  day7 character varying(255) NOT NULL,
+  day8 character varying(255) NOT NULL,
+  day9 character varying(255) NOT NULL,
+  day10 character varying(255) NOT NULL,
+  day11 character varying(255) NOT NULL,
+  day12 character varying(255) NOT NULL,
+  day13 character varying(255) NOT NULL,
+  day14 character varying(255) NOT NULL,
+  day15 character varying(255) NOT NULL,
+  day16 character varying(255) NOT NULL,
+  day17 character varying(255) NOT NULL,
+  day18 character varying(255) NOT NULL,
+  day19 character varying(255) NOT NULL,
+  day20 character varying(255) NOT NULL,
+  day21 character varying(255) NOT NULL,
+  day22 character varying(255) NOT NULL,
+  day23 character varying(255) NOT NULL,
+  day24 character varying(255) NOT NULL,
+  day25 character varying(255) NOT NULL,
+  day26 character varying(255) NOT NULL,
+  day27 character varying(255) NOT NULL,
+  day28 character varying(255) NOT NULL,
+  day29 character varying(255) NOT NULL,
+  day30 character varying(255) NOT NULL,
+  day31 character varying(255) NOT NULL,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  deleted_at timestamp without time zone,
+  created_from_ip character varying(45) DEFAULT NULL::character varying,
+  updated_from_ip character varying(45) DEFAULT NULL::character varying,
+  "yearEntity_id" bigint NOT NULL,
+  CONSTRAINT statistics_monthly_pkey PRIMARY KEY (id),
+  CONSTRAINT "FK_DA2703A94DFDCFB" FOREIGN KEY ("yearEntity_id")
+      REFERENCES statistics_year (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
 );
 
+-- Index: "IDX_DA2703A94DFDCFB"
 
-ALTER TABLE "statistics_monthly" ADD CONSTRAINT "FK_DA2703A94DFDCFB" FOREIGN KEY ("yearEntity_id") REFERENCES "statistics_year" ("id");
+-- DROP INDEX "IDX_DA2703A94DFDCFB";
 
+CREATE INDEX "IDX_DA2703A94DFDCFB" ON statistics_monthly USING btree ("yearEntity_id");
 commit;
