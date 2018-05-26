491436e6b5b04931e17184ce43eb0961c0b7ee59
Carlos Mendoza <inhack20@gmail.com>
2018-05-26 16:45:01 -0400

    refactor(correos): se agrego modelo base de correo.

---
+++ Model/Email/ORM/ModelEmailQueue.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Tecnoready\Common\Model\Email\ORM;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Modelo de email almacenado para generar el correo posteriormente y enviarlo
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ * @ORM\MappedSuperclass()
+ */
+abstract class ModelEmailQueue 
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="string", length=36)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    protected $id;
+    
+    const STATUS_NOT_SENT = "not_sent";
+    const STATUS_SENT = "sent";
+    const STATUS_FAIL = "fail";
+    
+    const ATTACH_DOCUMENTS = "attach_documents";
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subject", type="json_array")
+     */
+    protected $subject;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="from_email", type="json_array", length=255)
+     */
+    protected $fromEmail;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="to_email", type="json_array")
+     */
+    protected $toEmail;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text")
+     */
+    protected $body;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255)
+     */
+    protected $status;
+    
+    /**
+     * @var string $environment
+     *
+     * @ORM\Column(name="environment", type="string", nullable=true)
+     */
+    protected $environment;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="attachs", type="json_array")
+     */
+    protected $attachs;
+    
+    use \Tecnoready\Common\Model\Traits\ExtraDataTrait;
+    
+    public function __construct() {
+        $this->extraData = [];
+    }
+    
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    public function getFromEmail() {
+        return $this->fromEmail;
+    }
+
+    public function getToEmail() {
+        return $this->toEmail;
+    }
+
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function getEnvironment() {
+        return $this->environment;
+    }
+
+    public function getAttachs() {
+        return $this->attachs;
+    }
+
+    public function setSubject($subject) {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function setFromEmail($fromEmail) {
+        $this->fromEmail = $fromEmail;
+        return $this;
+    }
+
+    public function setToEmail($toEmail) {
+        $this->toEmail = $toEmail;
+        return $this;
+    }
+
+    public function setBody($body) {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function setEnvironment($environment) {
+        $this->environment = $environment;
+        return $this;
+    }
+
+    public function setAttachs($attachs) {
+        $this->attachs = $attachs;
+        return $this;
+    }
+}
