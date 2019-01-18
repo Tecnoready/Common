2b2856cb145c259fce7cb7db1e6963a8e1bf6540
Carlos Mendoza <inhack20@gmail.com>
2019-01-18 15:21:44 -0400

    feat(tabs): se agrego modelos de tabs

---
+++ Model/Tab/TabContent.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Tecnoready\Common\Model\Tab;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Contenido de tab
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class TabContent
+{
+    const TAB = "_tbx";
+    
+    private $id;
+    private $icon;
+    private $title;
+    private $order;
+    private $options;
+    private $active = false;
+    
+    /**
+     * @var Tab
+     */
+    private $tabRoot;
+
+    public function __construct(array $options = []) 
+    {
+        $this->setOptions($options);
+    }
+    
+    /**
+     * Opciones de la tab
+     * @param array $options
+     * @return \Atechnologies\ToolsBundle\Model\Core\Tab\TabContent
+     */
+    public function setOptions(array $options = []) 
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            "add_content_div" => true,
+            "url" => null,
+        ]);
+        $resolver->setRequired(["url","template"]);
+        $this->options = $resolver->resolve($options);
+        
+        return $this;
+    }
+    
+    /**
+     * Busca una opcion
+     * @param type $name
+     * @return type
+     */
+    public function getOption($name) 
+    {
+        $value = $this->options[$name];
+        if($value === null && $name === "url"){
+            $value = $this->tabRoot->getRootUrl();
+        }
+        return $value;
+    }
+    
+    /**
+     * getUrl
+     * @return url
+     */
+    public function getUrl() 
+    {
+        return $this->options["url"];
+    }
+
+    /**
+     * getOrder
+     * @return order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * setOrder
+     * @param  order
+     */
+    public function setOrder($order) 
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    /**
+     * getId
+     * @return id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * setId
+     * @param  id
+     */
+    public function setId($id) 
+    {
+        $this->id = $id;
+        return $this;
+    }
+    
+    /**
+     * getActive
+     * @return active
+     */
+    public function getActive() 
+    {
+        return $this->active;
+    }
+    
+    /**
+     * setActive
+     * @param  [type]
+     */
+    public function setActive($active) 
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    /**
+     * setIcon
+     * @param  icon
+     */
+    public function setIcon($icon) 
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    /**
+     * getIcon
+     * @return icon
+     */
+    public function getIcon() 
+    {
+        return $this->icon;
+    }
+    
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+    
+    public function setTabRoot(Tab $tabRoot)
+    {
+        $this->tabRoot = $tabRoot;
+        return $this;
+    }
+                
+    /**
+     * Representacion de la tab en arary
+     * @return array
+     */
+    public function toArray() 
+    {
+        $data = [
+            "id" => $this->id,
+            "title" => $this->title,
+            "active" => $this->active,
+            "icon" => $this->icon,
+            "options" => $this->options,
+        ];
+
+        return $data;
+    }
+}
