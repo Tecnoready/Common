ca55c646bdb2ce499cb513f963a427779d2cf16f
Maximo Sojo <maxsojo13@gmail.com>
2019-11-26 13:47:42 -0400

    refactor(TXTAdapter): Se actualizar adaptador de txt.

--- Service/Template/Adapter/TXTAdapter.php
+++ Service/Template/Adapter/TXTAdapter.php
@@ -6,6 +6,7 @@ use Tecnoready\Common\Service\Template\AdapterInterface;
 use Tecnoready\Common\Model\Template\TemplateInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use RuntimeException;
+use Twig_Environment;
 
 /**
  * Adaptador para exportar a txt
@@ -14,14 +15,52 @@ use RuntimeException;
  */
 class TXTAdapter implements AdapterInterface
 {
+    /**
+     * @var Twig_Environment 
+     */
+    private $twig;
+
+    /**
+     * Opciones de configuracion
+     * @var array
+     */
+    private $options;
+
+    private $fname;
+    private $variables;
+
+    public function __construct(Twig_Environment $twig,array $options = [])
+    {
+        $this->twig = $twig;
+        $resolver = new OptionsResolver();
+        $tmpDir = sys_get_temp_dir();
+        $resolver->setDefaults([
+            "tmpDir" => $tmpDir,//Carpeta temporal
+        ]);
+        $this->options = $resolver->resolve($options);
+        
+    }
+
     public function render(TemplateInterface $template, array $variables)
     {
-        return true;
+        $this->variables = $variables;
+        
+        $this->fname = tempnam(null, "content".".php");
+        $fh = fopen($this->fname, "a");
+            fwrite($fh,$template->getContent());
+        fclose($fh);
+
+        return $this->fname;
     }
     
-    public function compile($filename, $spreadsheet, array $parameters)
+    public function compile($filename, $string, array $parameters)
     {
-        return true;
+        extract($this->variables);
+        $fh = fopen($filename, "w");
+            include $this->fname;
+        fclose($fh);
+        
+        return $filename;
     }
 
     public function getDefaultParameters()
